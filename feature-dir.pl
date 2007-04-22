e0f9ae2dc402bef08e8a8409624152e7c2200a03
Jamie Cameron <jcameron@webmin.com>
2007-04-22 06:12:08 +0000

    Don't create cgi-bin for sub-domains

--- feature-dir.pl
+++ feature-dir.pl
@@ -263,7 +263,7 @@ sub virtual_server_directories
 local $tmpl = &get_template($_[0]->{'template'});
 local $perms = $tmpl->{'web_html_perms'};
 return ( $d->{'subdom'} ? ( ) : ( [ &public_html_dir($_[0], 1), $perms ] ),
-         [ 'cgi-bin', $perms ],
+         $d->{'subdom'} ? ( ) : ( [ 'cgi-bin', $perms ] ),
          [ 'logs', '750' ],
          [ $config{'homes_dir'}, '755' ] );
 }
