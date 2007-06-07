5590580062bd44e17503bbfaa1ecd89085896b8f
Jamie Cameron <jcameron@webmin.com>
2007-06-07 23:11:52 +0000

    Prevent concurrent collectinfo.pl runs

--- collectinfo.pl
+++ collectinfo.pl
@@ -6,8 +6,15 @@ package virtual_server;
 $main::no_acl_check++;
 require './virtual-server-lib.pl';
 
+# Make sure we are not already running
+if (&test_lock($collected_info_file)) {
+	print "Already running\n";
+	exit(0);
+	}
+&lock_file($collected_info_file);
+
 $info = &collect_system_info();
 if ($info) {
 	&save_collected_info($info);
 	}
-
+&unlock_file($collected_info_file);
