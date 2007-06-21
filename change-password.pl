55388e07911e8519367c4fe7a848f33f4c6bd4e3
Jamie Cameron <jcameron@webmin.com>
2007-06-21 05:27:02 +0000

    Updated change-password.pl to make it more suitable for calling from Usermin

--- change-password.pl
+++ change-password.pl
@@ -1,14 +1,7 @@
 #!/usr/local/bin/perl
-# Changes the password of some Virtualmin user. Designed to be called
-# setuid, by non-root users.
+# Changes the password of some Virtualmin user. Designed to be called from
+# Usermin's Change Passwords module.
 
-#open(DEBUG, ">/tmp/change-password.debug");
-open(DEBUG, ">/dev/null");
-#close(STDERR);
-#open(STDERR, ">&DEBUG");
-select(DEBUG); $| = 1; select(STDOUT);
-
-print DEBUG "Running as $<\n";
 $no_acl_check++;
 $ENV{'WEBMIN_CONFIG'} ||= "/etc/webmin";
 $ENV{'WEBMIN_VAR'} ||= "/var/webmin";
@@ -16,39 +9,40 @@ if ($0 =~ /^(.*\/)[^\/]+$/) {
 	chdir($1);
 	}
 chop($pwd = `pwd`);
-print DEBUG "Before setting \$0 (from $0)\n";
 $0 = "$pwd/change-password.pl";
-print DEBUG "Before require\n";
-eval "require './virtual-server-lib.pl';";
-print DEBUG $@;
-print DEBUG "After require\n";
+require './virtual-server-lib.pl';
 $< == 0 || die "change-password.pl must be run as root";
-print DEBUG "Password preliminary checks\n";
 
 # Read inputs
 $| = 1;
-print "Username: ";
-chop($username = <STDIN>);
+if ($ARGV[0]) {
+	$username = $ARGV[0];
+	}
+else {
+	print "Username: ";
+	chop($username = <STDIN>);
+	}
 print "Old password: ";
 chop($oldpass = <STDIN>);
 print "New password: ";
 chop($newpass = <STDIN>);
-$username || die "No username given";
-print DEBUG "username=$username oldpass=$oldpass newpass=$newpass\n";
+if (!$username) {
+	&error_exit("No username given");
+	}
 sleep(5);		# To prevent brute force attacks
 
 # Find the user
 $d = &get_user_domain($username);
-$d || die "Not a Virtualmin user";
+$d || &error_exit("Not a Virtualmin user");
 @users = &list_domain_users($d);
 ($user) = grep { $_->{'user'} eq $username } @users;
-$user || die "Not a Virtualmin user in $d->{'dom'}";
+$user || &error_exit("Not a Virtualmin user in $d->{'dom'}");
 $olduser = { %$user };
 
 if ($user->{'domainowner'}) {
 	# This is the domain owner, so changing his password means updating
 	# all features
-	$d->{'pass'} eq $oldpass || die "Wrong password";
+	$d->{'pass'} eq $oldpass || &error_exit("Wrong password");
 	&set_all_null_print();
 	foreach my $d (&get_domain_by("user", $username)) {
 		$oldd = { %$d };
@@ -80,7 +74,8 @@ if ($user->{'domainowner'}) {
 else {
 	# Can just change the user
 	if (defined($user->{'plainpass'})) {
-		$user->{'plainpass'} eq $oldpass || die "Wrong password";
+		$user->{'plainpass'} eq $oldpass ||
+			&error_exit("Wrong password");
 		}
 	$user->{'passmode'} = 3;
 	$user->{'plainpass'} = $newpass;
@@ -91,3 +86,8 @@ else {
 print "Password changed for $username in $d->{'dom'}\n";
 exit(0);
 
+sub error_exit
+{
+print STDERR @_,"\n";
+exit(1);
+}
