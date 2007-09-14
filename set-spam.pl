d3827d7b290ee3d1083dee850d200b70a8570017
Jamie Cameron <jcameron@webmin.com>
2007-09-14 04:58:38 +0000

    More work on new virus scanner command-line API

--- set-spam.pl
+++ set-spam.pl
@@ -33,10 +33,56 @@ while(@ARGV > 0) {
 		&usage();
 		}
 	}
-$virus_scanner || $spam_scanner || $show || &usage("Nothing to do");
 
-# XXX do it
+# Validate inputs
+$virus_scanner || $spam_client || $show || &usage("Nothing to do");
+if ($spam_client) {
+	&has_command($spam_client) ||
+	    &usage("SpamAssassin client program $spam_client does not exist");
+	}
+if ($virus_scanner) {
+	local ($cmd, @args) = &split_quoted_string($virus_scanner);
+	&has_command($cmd) ||
+		&usage("Virus scanning command $cmd does not exist");
+	$err = &test_virus_scanner($virus_scanner);
+	$err && &usage("Virus scanner failed : $err");
+	}
+
+if ($spam_client) {
+	print "Updating all virtual servers with new SpamAssassin client ..\n";
+	&save_global_spam_client($spam_client);
+	print ".. done\n\n";
+	}
+
+if ($virus_scanner) {
+	print "Updating all virtual servers with new virus scanner ..\n";
+	&save_global_virus_scanner($virus_scanner);
+	print ".. done\n\n";
+	}
+
+&run_post_actions();
 
 if ($show) {
 	# Show current settings
+	if ($config{'spam'}) {
+		$client = &get_global_spam_client();
+		print "SpamAssassin client: $client\n";
+		}
+	if ($config{'virus'}) {
+		$scanner = &get_global_virus_scanner();
+		print "Virus scanner: $scanner\n";
+		}
 	}
+
+sub usage
+{
+print "$_[0]\n\n" if ($_[0]);
+print "Changes the spam and virus scanning programs for all domains.\n";
+print "\n";
+print "usage: set-spam.pl [--use-spamassassin | --use-spamc]\n";
+print "                   [--use-clamscan | --use-clamdscan |\n";
+print "                    --use-virus command]\n";
+print "                   [--show]\n";
+exit(1);
+}
+
