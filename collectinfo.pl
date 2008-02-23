1399037b6bcf4c246374e6f59b4599585b54989f
Jamie Cameron <jcameron@webmin.com>
2008-02-23 06:42:06 +0000

    Don't diff collectinfo data file

--- collectinfo.pl
+++ collectinfo.pl
@@ -13,6 +13,8 @@ if (&test_lock($collected_info_file)) {
 	print "Already running\n";
 	exit(0);
 	}
+local $gconfig{'logfiles'} = 0;		# Don't diff collected file
+local $gconfig{'logfullfiles'} = 0;
 &lock_file($collected_info_file);
 
 $info = &collect_system_info();
