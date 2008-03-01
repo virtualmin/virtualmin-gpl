79d5172a0ab7218c2b07caa16657fcb61256e661
Jamie Cameron <jcameron@webmin.com>
2008-03-01 18:13:48 +0000

    New zenphoto and phpmyadmin versions, with fixes

--- scripts/phpmyadmin.pl
+++ scripts/phpmyadmin.pl
@@ -19,7 +19,7 @@ return "A browser-based MySQL database management interface.";
 # script_phpmyadmin_versions()
 sub script_phpmyadmin_versions
 {
-return ( "2.11.4", "2.9.2", "2.8.2.4" );
+return ( "2.11.5", "2.9.2", "2.8.2.4" );
 }
 
 sub script_phpmyadmin_version_desc
