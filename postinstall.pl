3f1b3f62f1319da45b2a8433cdba9e662967fc1c
Jamie Cameron <jcameron@webmin.com>
2008-03-17 19:07:55 +0000

    New osticket version

--- postinstall.pl
+++ postinstall.pl
@@ -344,7 +344,7 @@ foreach my $m ("mysql", "postgresql", "ldap-client", "ldap-server",
 	       "ldap-useradmin", $module_name) {
 	local $mdir = "$config_directory/$m";
 	if (-d $mdir) {
-		&set_ownership_permissions(undef, undef, 0751,
+		&set_ownership_permissions(undef, undef, 0711,
 					   $mdir, "$mdir/config");
 		}
 	}
