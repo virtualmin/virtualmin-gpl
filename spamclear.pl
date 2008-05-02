d54c3b0c99252ef93fa54e2896cb5365187a2430
Jamie Cameron <jcameron@webmin.com>
2008-05-02 22:53:50 +0000

    Debugging flags for spamclear.pl

--- spamclear.pl
+++ spamclear.pl
@@ -7,6 +7,17 @@ $no_virtualmin_plugins = 1;
 require './virtual-server-lib.pl';
 &foreign_require("mailboxes", "mailboxes-lib.pl");
 
+# Parse command-line args
+while(@ARGV) {
+	$a = shift(@ARGV);
+	if ($a eq "--debug") {
+		$debug = 1;
+		}
+	else {
+		$ucheck{$a}++;
+		}
+	}
+
 # hack to force correct DBM mode
 $temp = &transname();
 &make_dir($temp, 0700);
@@ -14,16 +25,25 @@ $temp = &transname();
 
 foreach $d (&list_domains()) {
 	# Is clearing enabled?
-	next if (!$d->{'spam'});
+	if (!$d->{'spam'}) {
+		print STDERR "$d->{'dom'}: spam filtering is not enabled\n"
+			if ($debug);
+		next;
+		}
 	$auto = &get_domain_spam_autoclear($d);
-	next if (!$auto);
-	#print STDERR "checking domain $d->{'dom'}\n";
+	if (!$auto) {
+		print STDERR "$d->{'dom'}: spam clearing is not enabled\n"
+			if ($debug);
+		next;
+		}
+	print STDERR "$d->{'dom'}: finding mailboxes\n" if ($debug);
 
 	# Check all mailboxes
 	@users = &list_domain_users($d, 0, 1, 1, 1);
 	foreach $u (@users) {
 		# Find spam folder
-		#print STDERR "checking user $u->{'user'}\n";
+		next if (keys %ucheck && !$ucheck{$u->{'user'}});
+		print STDERR " $u->{'user'}: finding folders\n" if ($debug);
 		@uinfo = ( $u->{'user'}, $u->{'pass'}, $u->{'uid'},
 			   $u->{'gid'}, undef, undef, $u->{'real'},
 			   $u->{'home'}, $u->{'shell'} );
@@ -31,8 +51,12 @@ foreach $d (&list_domains()) {
 		foreach $fn ("spam", "virus") {
 			($folder) = grep { $_->{'file'} =~ /\/(\.?)\Q$fn\E$/i &&
 					   $_->{'index'} != 0 } @folders;
-			next if (!$folder);
-			#print STDERR "found spam folder $folder->{'file'}\n";
+			if (!$folder) {
+				print STDERR "  $u->{'user'}: no $fn folder\n"
+					if ($debug);
+				next;
+				}
+			print STDERR "  $u->{'user'}: $fn folder is $folder->{'file'}\n" if ($debug);
 
 			# Verify the index on the spam folder
 			if ($folder->{'type'} == 0) {
@@ -55,7 +79,8 @@ foreach $d (&list_domains()) {
 			# Get email in the folder, and check criteria
 			my @mail = &mailboxes::mailbox_list_mails(
 				undef, undef, $folder, 1);
-			#print STDERR "mail count = ",scalar(@mail),"\n";
+			print STDERR "  $u->{'user'}: mail count ",
+				     scalar(@mail),"\n" if ($debug);
 			my @delmail;
 			if ($auto->{'days'}) {
 				# Find mail older than some number of days
@@ -73,7 +98,9 @@ foreach $d (&list_domains()) {
 				# Find oldest mail that is too large
 				$needsize = &mailboxes::folder_size($folder) -
 					    $auto->{'size'};
-				#print STDERR "size is ",&mailboxes::folder_size($folder),"\n";
+				print STDERR "  $u->{'user'}: mail size ",
+					&mailboxes::folder_size($folder),"\n"
+					if ($debug);
 				foreach $m (@mail) {
 					last if ($needsize <= 0);
 					push(@delmail, $m);
@@ -84,7 +111,9 @@ foreach $d (&list_domains()) {
 
 			# Delete any mail found
 			if (@delmail) {
-				#print STDERR "deleting ",scalar(@delmail)," messages\n";
+				print STDERR "  $u->{'user'}: deleting ",
+					scalar(@delmail)," messages\n"
+					if ($debug);
 				&mailboxes::mailbox_delete_mail(
 					$folder, reverse(@delmail));
 				}
