bcc9a69f5a4475ae4f8384c7ee7d080a4fd5eed5
Jamie Cameron <jcameron@webmin.com>
2008-08-06 07:08:11 +0000

    Quick script to copy mail and convert formats

---
+++ copy-mailbox.pl
@@ -0,0 +1,89 @@
+#!/usr/local/bin/perl
+
+=head1 copy-mailbox.pl
+
+Copy mail from one location to another, perhaps converting formats.
+
+The source mail is specified with the C<--source> flag, and the destination
+with the C<--dest> parameter. Both must be followed by a full path, which
+can end with a / to indicate that it is in Maildir format.
+
+By default email is just coped, but the C<--delete> flag can be given to
+have it moved instead.
+
+=cut
+
+package virtual_server;
+if (!$module_name) {
+	$main::no_acl_check++;
+	$ENV{'WEBMIN_CONFIG'} ||= "/etc/webmin";
+	$ENV{'WEBMIN_VAR'} ||= "/var/webmin";
+	if ($0 =~ /^(.*\/)[^\/]+$/) {
+		chdir($1);
+		}
+	chop($pwd = `pwd`);
+	$0 = "$pwd/copy-mailbox.pl";
+	require './virtual-server-lib.pl';
+	$< == 0 || die "list-mailbox.pl must be run as root";
+	}
+
+# Parse command-line args
+$delete = 0;
+while(@ARGV > 0) {
+	local $a = shift(@ARGV);
+	if ($a eq "--source") {
+		$src = shift(@ARGV);
+		}
+	elsif ($a eq "--dest") {
+		$dest = shift(@ARGV);
+		}
+	elsif ($a eq "--delete") {
+		$delete = 1;
+		}
+	else {
+		&usage();
+		}
+	}
+$src && $dest || &usage("Missing --source or --dest parameter");
+
+# Get the folders
+&foreign_require("mailboxes", "mailboxes-lib.pl");
+$srcfolder = { 'file' => $src,
+	       'type' => 0 };
+if ($src =~ /\/$/ || -d $src) {
+	$srcfolder->{'file'} =~ s/\/$//;
+	$srcfolder->{'type'} = 1;
+	}
+$destfolder = { 'file' => $dest,
+	        'type' => 0 };
+if ($dest =~ /\/$/ || -d $dest) {
+	$destfolder->{'file'} =~ s/\/$//;
+	$destfolder->{'type'} = 1;
+	}
+-d $srcfolder->{'file'} || -r $srcfolder->{'file'} ||
+	&usage("Source folder $srcfolder->{'file'} does not exist");
+
+# Copy mail
+$sz = &mailboxes::folder_size($srcfolder);
+$count = &mailboxes::mailbox_folder_size($srcfolder);
+print $delete ? "Moving" : "Copying";
+print " $count messages totalling ",&nice_size($sz)," ...\n";
+if ($delete) {
+	&mailboxes::mailbox_move_folder($srcfolder, $destfolder);
+	}
+else {
+	&mailboxes::mailbox_copy_folder($srcfolder, $destfolder);
+	}
+print "... done\n";
+
+sub usage
+{
+print "$_[0]\n\n" if ($_[0]);
+print "Copies or moves mail from one file or directory to another.\n";
+print "\n";
+print "usage: copy-mailbox.pl --source file\n";
+print "                       --dest file\n";
+print "                       [--delete]\n";
+exit(1);
+}
+
