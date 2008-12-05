d497a364ed27cae88ee1e693753c7ecdfe82c4a7
Jamie Cameron <jcameron@webmin.com>
2008-12-05 18:15:33 +0000

    New squirrelmail version

--- scripts/squirrelmail.pl
+++ scripts/squirrelmail.pl
@@ -18,7 +18,7 @@ return "SquirrelMail is a standards-based webmail package written in PHP";
 # script_squirrelmail_versions()
 sub script_squirrelmail_versions
 {
-return ( "1.4.16" );
+return ( "1.4.17" );
 }
 
 sub script_squirrelmail_version_desc
