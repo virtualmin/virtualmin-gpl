99799c2671a6939ebb4ee3d1cc3eb183c7cabe5c
Jamie Cameron <jcameron@webmin.com>
2009-01-20 08:10:57 +0000

    Completed get-template

--- get-template.pl
+++ get-template.pl
@@ -4,7 +4,22 @@
 
 Outputs all settings in a template.
 
-XXX
+This command can be used to output all settings in a Virtualmin template,
+specified either by name with the C<--name> parameter, or by ID with the
+C<--id> flag. By default only values actually set in the template are output,
+but you can add the C<--inherited> flag to include settings from the default
+template too.
+
+By default all settings are output, formatted like :
+
+  aliascopy: 0
+  aliasdomslimit: none
+  aliaslimit: none
+  etc..
+
+Any newlines in multi-line values are converted to \n for the output. However,
+if you select to output just a single setting with the C<--value> option
+followed by a setting name, newlines in it's value will B<not> be converted.
 
 =cut
 
@@ -35,6 +50,9 @@ while(@ARGV > 0) {
 	elsif ($a eq "--inherited") {
 		$inherited = 1;
 		}
+	elsif ($a eq "--value") {
+		$keyname = shift(@ARGV);
+		}
 	else {
 		&usage();
 		}
@@ -57,12 +75,19 @@ if ($inherited) {
 	$tmpl = &get_template($tmpl->{'id'});
 	}
 
-# Dump the contents
-foreach my $k (sort { $a cmp $b } keys %$tmpl) {
-	$tmpl->{$k} =~ s/\r//g;
-	$tmpl->{$k} =~ s/\\/\\\\/g;
-	$tmpl->{$k} =~ s/\n/\\n/g;
-	print "$k: $tmpl->{$k}\n";
+if ($keyname) {
+	# Print just one key
+	print $tmpl->{$keyname};
+	print "\n" if ($tmpl->{$keyname} !~ /\n$/);
+	}
+else {
+	# Dump the contents
+	foreach my $k (sort { $a cmp $b } keys %$tmpl) {
+		$tmpl->{$k} =~ s/\r//g;
+		$tmpl->{$k} =~ s/\\/\\\\/g;
+		$tmpl->{$k} =~ s/\n/\\n/g;
+		print "$k: $tmpl->{$k}\n";
+		}
 	}
 
 sub usage
@@ -71,6 +96,7 @@ print "$_[0]\n\n" if ($_[0]);
 print "Outputs all settings in some virtual server template.\n";
 print "\n";
 print "usage: get-template.pl --name template-name | --id template-id\n";
+print "                       [--value name]\n";
 print "                       [--inherited]\n";
 exit(1);
 }
