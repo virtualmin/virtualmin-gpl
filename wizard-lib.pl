d7104afa8ad49d8b7b9f0e5f56c60dab38245a53
Jamie Cameron <jcameron@webmin.com>
2009-04-19 21:24:40 +0000

    More on wizards

--- wizard-lib.pl
+++ wizard-lib.pl
@@ -1,7 +1,5 @@
 # Functions for the post-install wizard
 
-@wizard_steps = ( "intro", "virus", "spam", "mysql" );
-
 # If the wizard should be invoked, returns a URL for it. For calling by themes
 sub wizard_redirect
 {
@@ -9,6 +7,52 @@ if (!$config{'wizard_run'} && $config{'first_version'} >= 3.69) {
 	return "/$module_name/wizard.cgi";
 	}
 return undef;
+}
+
+sub get_wizard_steps
+{
+return ( "intro",
+	 $config{'virus'} ? ( "virus" ) : ( ),
+	 $config{'spam'} ? ( "spam" ): ( ),
+	 $config{'mysql'} ? ( "mysql" ) : ( ) );
+}
+
+sub wizard_show_intro
+{
+print &ui_table_row(undef,
+	$text{'wizard_intro'}, 2);
+}
+
+sub wizard_show_virus
+{
+print &ui_table_row(undef,
+	$text{'wizard_virus'}, 2);
+local $cs = &check_clamd_status();
+if ($cs != -1) {
+	print &ui_table_row($text{'wizard_clamd'},
+		&ui_radio("clamd", $cs ? 1 : 0,
+			  [ [ 1, $text{'wizard_clamd1'}."<br>" ],
+			    [ 0, $text{'wizard_clamd0'} ] ]));
+	}
+else {
+	print &ui_table_row($text{'wizard_clamdnone'});
+	}
+}
+
+sub wizard_parse_virus
+{
+local ($in) = @_;
+if (defined($in->{'clamd'})) {
+	local $cs = &check_clamd_status();
+	if ($in->{'clamd'} && !$cs) {
+		# Enable if needed
+		# XXX
+		}
+	elsif (!$in->{'clamd'} && $cs) {
+		# Disable if needed
+		# XXX
+		}
+	}
 }
 
 1;
--- wizard.cgi
+++ wizard.cgi
@@ -12,18 +12,37 @@ if ($in{'cancel'}) {
 	return;
 	}
 
+@wizard_steps = &get_wizard_steps();
 $step = $wizard_steps[$in{'step'} || 0];
 
 if ($in{'parse'}) {
 	# Call the parse function for this step, which may return an error.
 	# If so, re-display the page .. otherwise, re-direct
+	$pfunc = "wizard_parse_".$step;
+	if (defined(&$pfunc)) {
+		$err = &$pfunc(\%in);
+		}
+	if (!$err) {
+		# Worked, show next step
+		&redirect("wizard.cgi?step=".($in{'step'}+1));
+		}
 	}
 
-&ui_print_header($text{'wizard_'.$step}, $text{'wizard_title'}, "");
+&ui_print_header($text{'wizard_title_'.$step}, $text{'wizard_title'}, "");
 
 print &ui_form_start("wizard.cgi", "post");
 print &ui_hidden("step", $in{'step'});
+print &ui_hidden("parse", 1);
+if ($err) {
+	print "<b><font color=#ff0000>$err</font></b><p>\n";
+	}
+print &ui_table_start(undef, "width=100%", 2);
 
+# Show step-specific inputs
+$ffunc = "wizard_show_".$step;
+&$ffunc();
+
+print &ui_table_end();
 print &ui_form_end([ [ undef, $text{'wizard_next'} ],
 		     undef,
 		     [ "cancel", $text{'wizard_cancel'} ] ]);
