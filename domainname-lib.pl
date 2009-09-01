f8994437a2ff34f48ea4f0bf066ee72aff9a96ac
Jamie Cameron <jcameron@webmin.com>
2009-09-01 16:16:57 +0000

    New opengoo version

--- domainname-lib.pl
+++ domainname-lib.pl
@@ -5,13 +5,13 @@
 # Returns a domain name shorten to the display max
 sub shorten_domain_name
 {
-local ($d) = @_;
-local $show = &show_domain_name($d->{'showdom'} || $d->{'dom'});
+my ($d) = @_;
+my $show = &show_domain_name($d->{'showdom'} || $d->{'dom'});
 
 # Split into characters, some of which may be HTML entities that are multiple
 # letters in the string (like &foo; or &#55;) but appear as one letter
-local @chars;
-local $tosplit = $show;
+my @chars;
+my $tosplit = $show;
 if ($tosplit =~ /\&|;/) {
 	while($tosplit =~ s/^(\&[^;]+;)// ||
 	      $tosplit =~ s/^(.)//) {
@@ -22,10 +22,10 @@ else {
 	@chars = split(//, $tosplit);
 	}
 
-local $rv;
+my $rv;
 if ($config{'name_max'} && scalar(@chars) > $config{'name_max'}) {
 	# Show first and last max/2 chars, with ... between.
-	local $s = int($config{'name_max'} / 2);
+	my $s = int($config{'name_max'} / 2);
 	$rv = join("", @chars[0 .. $s-1])."...".
 	      join("", @chars[$#chars-$s .. $#chars]);
 	}
@@ -41,8 +41,8 @@ return $rv;
 # takes IDN encoding into account
 sub show_domain_name
 {
-local $name = ref($_[0]) ? $_[0]->{'dom'} : $_[0];
-local $spacer;
+my $name = ref($_[0]) ? $_[0]->{'dom'} : $_[0];
+my $spacer;
 if ($name =~ s/^(\s+)//) {
 	$spacer = $1;
 	}
@@ -56,9 +56,9 @@ if ($name =~ /^xn--/ || $name =~ /\.xn--/) {
 			  map { decode_punycode($_) } split(/\./, $name));
 		if ($ENV{'MINISERV_CONFIG'}) {
 			# In browser, so convert to entity format for HTML
-			local $ename;
+			my $ename;
 			foreach my $c (split(//, $name)) {
-				local $o = ord($c);
+				my $o = ord($c);
 				$ename .= $o > 255 ? "&#$o;" : $c;
 				}
 			$name = $ename;
@@ -72,7 +72,7 @@ return $spacer.$name;
 # Returns an IDN-encoding domain name, where needed
 sub parse_domain_name
 {
-local $name = &entities_to_ascii($_[0]);
+my $name = &entities_to_ascii($_[0]);
 $name =~ s/^\s+//;	# Strip leading and trailing spaces from user input
 $name =~ s/\s+$//;
 if ($name !~ /^[a-z0-9\.\-\_]+$/i) {
@@ -93,7 +93,7 @@ return $name;
 # Expects parse_domain_name to have been already called.
 sub valid_domain_name
 {
-local ($name) = @_;
+my ($name) = @_;
 $name =~ /^[A-Za-z0-9\.\-]+$/ || return $text{'setup_edomain'};
 $name =~ /^\./ && return $text{'setup_edomain2'};
 $name =~ /\.$/ && return $text{'setup_edomain2'};
