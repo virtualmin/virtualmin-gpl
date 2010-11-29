84c961b71d80b5e5b413be6ee28c76f6e0848220
Jamie Cameron <jcameron@webmin.com>
2010-11-28 18:51:14 -0800

    More work on provisioning client

--- provision-lib.pl
+++ provision-lib.pl
@@ -5,8 +5,35 @@ sub list_provision_features
 return ('dns', 'mysql');
 }
 
+# check_provision_login()
+# Validate that the selected provisioning system can be used and has the needed
+# features.
 sub check_provision_login
 {
+my ($ok, $msg) = &provision_api_call("list-provision-features", {}, 1);
+if (!$ok) {
+	# Request failed
+	return $msg;
+	}
+if (ref($msg) ne 'HASH') {
+	return "Invalid response from list-provision-features : $msg";
+	}
+if ($config{'provision_dns'}) {
+	# Make sure DNS is supported
+	$msg->{'dns'} || return $text{'provision_edns'};
+	$msg->{'dns'}->{'limit'} || return $text{'provision_ednslimit'};
+	$msg->{'dns'}->{'systems'} || return $text{'provision_ednssystems'};
+	}
+if ($config{'provision_mysql'}) {
+	# Make sure MySQL logins and DBs are supported
+	$msg->{'mysql'} && $msg->{'mysqldb'} ||
+		return $text{'provision_emysql'};
+	$msg->{'mysql'}->{'limit'} && $msg->{'mysqldb'}->{'limit'} ||
+		return $text{'provision_emysqllimit'};
+	$msg->{'mysql'}->{'systems'} && $msg->{'mysqldb'}->{'systems'} ||
+		return $text{'provision_emysqlsystems'};
+	}
+return undef;
 }
 
 # provision_api_call(command, &args, multiline)
@@ -19,7 +46,7 @@ my ($cmd, $args, $multiline) = @_;
 my ($out, $err);
 &http_download($config{'provision_server'}, $config{'provision_port'},
 	       "/server-manager/remote.cgi?program=".&urlize($cmd).
-	       join("", map { "&".$_."=".&urlize($args->{$_}) } (keys %$args).
+	       join("", map { "&".$_."=".&urlize($args->{$_}) } (keys %$args)).
 	       ($multiline ? "&multiline=&perl=" : ""),
 	       \$out, \$err, undef,
 	       $config{'provision_ssl'},
@@ -28,6 +55,18 @@ my ($out, $err);
 	       60, 0, 1);
 if ($err) {
 	return (0, $err);
+	}
+if ($multiline) {
+	# Parse perl format
+	my $rv = eval $out;
+	if ($@) {
+		return (0, "Invalid response format : $@");
+		}
+	return (1, $rv);
+	}
+else {
+	# Plain text format
+	return (1, $out);
 	}
 }
 
---
+++ save_provision.cgi
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+# Save provisioning settings
+
+require './virtual-server-lib.pl';
+&can_edit_templates() || &error($text{'provision_ecannot'});
+&error_setup($text{'provision_err'});
+&ReadParse();
+
+# Validate and store inputs
+foreach $f (&list_provision_features()) {
+	push(@oldpfeatures, $f) if ($config{'provision_'.$f});
+	push(@pfeatures, $f) if ($in{'provision_'.$f});
+	}
+if (@pfeatures) {
+	&to_ipaddress($in{'provision_server'}) ||
+	  defined(&to_ip6address) && &to_ip6address($in{'provision_server'}) ||
+	     &error($text{'provision_eserver'});
+	$in{'provision_port'} =~ /^[1-9][0-9]*$/ ||
+	     &error($text{'provision_eport'});
+	$in{'provision_user'} =~ /^[a-zA-Z0-9\.\-\_\@]+$/ ||
+	     &error($text{'provision_euser'});
+	$in{'provision_pass'} =~ /:/ &&
+	     &error($text{'provision_epass'});
+	}
+$config{'provision_server'} = $in{'provision_server'};
+$config{'provision_port'} = $in{'provision_port'};
+$config{'provision_ssl'} = $in{'provision_ssl'} || 0;
+$config{'provision_user'} = $in{'provision_user'};
+$config{'provision_pass'} = $in{'provision_pass'};
+foreach $f (&list_provision_features()) {
+	$config{'provision_'.$f} = $in{'provision_'.$f} || 0;
+	}
+
+&ui_print_header(undef, $text{'provision_title'}, "");
+
+# Check that provisioning works for the server and login
+&$first_print($text{'provision_checking', "<tt>$in{'provision_server'}</tt>"));
+$err = &check_provision_login();
+if ($err) {
+	&$second_print(&text('provision_echeck', $err));
+	goto FAILED:
+	}
+else {
+	&$second_print($text{'setup_done'});
+	}
+
+# If any domains exist that were provisioned old-style for mysql, complain
+# XXX
+
+# Get limits from the server and display
+# XXX
+
+# Save config and tell the user
+&$first_print($text{'provision_saving'});
+&save_module_config();
+&$second_print($text{'setup_done'});
+
+FAILED:
+&ui_print_footer("", $text{'index_return'});
+
