220e665a4bdd2de28ee71623b31e518252faac19
Jamie Cameron <jcameron@webmin.com>
2011-02-13 10:53:26 -0800

    New phpadmin version

--- scripts/phpmyadmin.pl
+++ scripts/phpmyadmin.pl
@@ -19,7 +19,7 @@ return "A browser-based MySQL database management interface.";
 # script_phpmyadmin_versions()
 sub script_phpmyadmin_versions
 {
-return ( "3.3.9.1", "2.11.11.2" );
+return ( "3.3.9.2", "2.11.11.3" );
 }
 
 sub script_phpmyadmin_version_desc
