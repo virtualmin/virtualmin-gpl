6b6583116d41543d5a062353eb62ec7cbeee6369
Jamie Cameron <jcameron@webmin.com>
2011-10-03 10:50:10 -0700

    New phppgadmin version

--- scripts/phppgadmin.pl
+++ scripts/phppgadmin.pl
@@ -19,7 +19,7 @@ return "A browser-based PostgreSQL database management interface.";
 # script_phppgadmin_versions()
 sub script_phppgadmin_versions
 {
-return ( "5.0.2" );
+return ( "5.0.3" );
 }
 
 sub script_phppgadmin_category
