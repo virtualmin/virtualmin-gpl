1b02b1f4abb8e44a4f00050826246f71ec25dff8
Jamie Cameron <jcameron@webmin.com>
2011-10-20 15:36:40 -0700

    Set max requests to effectively unlimited, to allow mod_fcgid to control it https://virtualmin.com/node/19921

--- php-lib.pl
+++ php-lib.pl
@@ -476,6 +476,8 @@ foreach my $v (&list_available_php_versions($d, $mode)) {
 				$common .= "PHP_FCGI_CHILDREN=$defchildren\n";
 				}
 			$common .= "export PHP_FCGI_CHILDREN\n";
+			$common .= "PHP_FCGI_MAX_REQUESTS=99999\n";
+			$common .= "export PHP_FCGI_MAX_REQUESTS\n";
 			}
 		&print_tempfile(PHP, $common);
 		if ($v->[1] =~ /-cgi$/) {
