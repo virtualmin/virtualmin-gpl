94c3649063fee87bf03daa6a1e43d1680b6bb980
Jamie Cameron <jcameron@webmin.com>
2012-01-26 17:04:43 -0800

    New dokuwiki version

--- scripts/dokuwiki.pl
+++ scripts/dokuwiki.pl
@@ -18,7 +18,7 @@ return "DokuWiki is a standards compliant, simple to use Wiki, mainly aimed at c
 # script_dokuwiki_versions()
 sub script_dokuwiki_versions
 {
-return ( "2011-05-25a" );
+return ( "2012-01-25" );
 }
 
 sub script_dokuwiki_category
