c9bab694e74d1cca9d282d80ccbdc06ac9bbe2a7
Jamie Cameron <jcameron@webmin.com>
2012-07-30 15:34:07 -0700

    New whmcs version

--- scripts/whmcs.pl
+++ scripts/whmcs.pl
@@ -24,7 +24,7 @@ return "WHMCS is an all-in-one client management, billing & support solution for
 # script_whmcs_versions()
 sub script_whmcs_versions
 {
-return ( "5.0.3" );
+return ( "5.1.2" );
 }
 
 sub script_whmcs_gpl
