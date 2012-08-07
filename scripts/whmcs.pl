14a59c3cdc60bb5218335ccc67333a9daf937d3c
Jamie Cameron <jcameron@webmin.com>
2012-08-07 11:23:31 -0700

    Fix WHMCS version detection

--- scripts/whmcs.pl
+++ scripts/whmcs.pl
@@ -473,8 +473,11 @@ return (1, "WHMCS directory and tables deleted.");
 
 sub script_whmcs_latest
 {
+local ($ver) = @_;
+local $sfx = $ver =~ /^(\d+)\.(\d+)\.(\d+)$/ ? ".$3" : "";
 return ( "http://www.whmcs.com/whats-new/#download",
-	 "WHMCS\\s+V(\\S+)\\s+Stable" );
+	 "WHMCS\\s+V(\\S+)\\s+Stable",
+	 undef, $sfx );
 }
 
 sub script_whmcs_site
