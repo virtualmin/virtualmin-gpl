2a4a7302a017b23e2a3c4974c2b2bc9847e3ad67
Jamie Cameron <jcameron@webmin.com>
2012-08-30 09:57:26 -0700

    Roll forward joe's accidental rollback

--- list-scripts.pl
+++ list-scripts.pl
@@ -57,6 +57,9 @@ while(@ARGV > 0) {
 	elsif ($a eq "--name-only") {
 		$nameonly = 1;
 		}
+	elsif ($a eq "--id-only") {
+		$idonly = 1;
+		}
 	else {
 		&usage("Unknown parameter $a");
 		}
@@ -120,6 +123,12 @@ foreach my $d (@doms) {
 		# Just show script type codes
 		foreach $sinfo (@scripts) {
 			print $sinfo->{'name'},"\n";
+			}
+		}
+	elsif ($idonly) {
+		# Just show script install IDs
+		foreach $sinfo (@scripts) {
+			print $sinfo->{'id'},"\n";
 			}
 		}
 	else {
