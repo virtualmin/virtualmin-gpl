16973b8e6cd014fa0c309a312f6d77d282f77ecb
Jamie Cameron <jcameron@webmin.com>
2013-04-23 16:37:14 -0700

    Install supported roundcube version in tests

--- functional-test.pl
+++ functional-test.pl
@@ -899,7 +899,7 @@ $gplscript_tests = [
 		      [ 'type', 'roundcube' ],
 		      [ 'path', '/roundcube' ],
 		      [ 'db', 'mysql '.$test_domain_db ],
-		      [ 'version', 'latest' ] ],
+		      [ 'version', '0.8.6' ] ],
 	},
 
 	# Check that it works
@@ -932,7 +932,7 @@ $gplscript_tests = [
 		      [ 'path', '/roundcube' ],
 		      [ 'db', 'mysql '.$test_domain_db.'_roundcube' ],
 		      [ 'newdb' ],
-		      [ 'version', 'latest' ] ],
+		      [ 'version', '0.8.6' ] ],
 	},
 
 	# Check that it works with it's own DB
@@ -1284,7 +1284,7 @@ $move_tests = [
 		      [ 'type', 'roundcube' ],
 		      [ 'path', '/roundcube' ],
 		      [ 'db', 'mysql '.$test_domain_db ],
-		      [ 'version', 'latest' ] ],
+		      [ 'version', '0.8.6' ] ],
 	},
 
 	# Move under the target
@@ -2761,7 +2761,7 @@ $incremental_tests = [
 		      [ 'type', 'roundcube' ],
 		      [ 'path', '/roundcube' ],
 		      [ 'db', 'mysql '.$test_domain_db ],
-		      [ 'version', 'latest' ] ],
+		      [ 'version', '0.8.6' ] ],
 	},
 
 	# Test that roundcube works before the backup
@@ -3706,7 +3706,7 @@ $webmin_tests = [
 
 	# Install a script via the web UI
 	{ 'command' => $webmin_wget_command.
-                       "${webmin_proto}://localhost:${webmin_port}/virtual-server/script_install.cgi?dom=\$DOMAIN_ID\\&script=roundcube\\&version=0.8.1\\&dir_def=0\\&dir=roundcube\\&passmode=\\&db=mysql_${test_domain_db}",
+                       "${webmin_proto}://localhost:${webmin_port}/virtual-server/script_install.cgi?dom=\$DOMAIN_ID\\&script=roundcube\\&version=0.8.6\\&dir_def=0\\&dir=roundcube\\&passmode=\\&db=mysql_${test_domain_db}",
 	  'grep' => [ '<body', '</body>', 'Install Script', 
 		      'Now installing RoundCube' ],
 	  'antigrep' => [ 'Error', 'failed' ],
