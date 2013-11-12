90b1353d442dc3e2f5eef9b287c178e57d3f6827
Jamie Cameron <jcameron@webmin.com>
2013-11-11 16:03:18 -0800

    new moodle version

--- scripts/django.pl
+++ scripts/django.pl
@@ -18,7 +18,7 @@ return "Django is a high-level Python Web framework that encourages rapid develo
 # script_django_versions()
 sub script_django_versions
 {
-return ( "1.5.5", "1.4.9" );
+return ( "1.6", "1.4.10" );
 }
 
 sub script_django_release
