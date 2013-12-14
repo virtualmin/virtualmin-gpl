5005b3324cc56254e4eaeda27937299ecbf7e9b3
Jamie Cameron <jcameron@webmin.com>
2013-12-14 11:16:42 -0800

    new django version

--- scripts/django.pl
+++ scripts/django.pl
@@ -18,7 +18,7 @@ return "Django is a high-level Python Web framework that encourages rapid develo
 # script_django_versions()
 sub script_django_versions
 {
-return ( "1.6", "1.4.10" );
+return ( "1.6.1", "1.4.10" );
 }
 
 sub script_django_release
