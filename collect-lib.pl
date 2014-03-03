0e38b19f50850a4735eaf95c318cd139f0db1bcd
Jamie Cameron <jcameron@webmin.com>
2014-03-03 15:01:27 -0800

    Sometime OpenVZ mounts from /dev/simfs https://virtualmin.com/node/32589

--- collect-lib.pl
+++ collect-lib.pl
@@ -84,7 +84,8 @@ if (&foreign_check("proc")) {
 				local ($t, $f) =
 					&mount::disk_space($m->[2], $m->[0]);
 				if (($m->[2] eq "simfs" || $m->[2] eq "vzfs" ||
-				     $m->[0] eq "/dev/vzfs") &&
+				     $m->[0] eq "/dev/vzfs" ||
+				     $m->[0] eq "/dev/simfs") &&
 				    $donevzfs{$t,$f}++) {
 					# Don't double-count VPS filesystems
 					next;
