5253bc65d1a7b9c3c098a075a77658d62e2192e9
Jamie Cameron <jcameron@webmin.com>
2014-08-07 22:24:44 -0700

    on second thoughts, just blacklist XFS from quota checking

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -10830,10 +10830,10 @@ sub has_quotacheck
 &foreign_require("mount");
 my @mounted = &mount::list_mounted();
 my ($hm) = grep { $_->[0] eq $config{'home_quotas'} } @mounted;
-return 1 if ($hm && $hm->[2] =~ /^ext/);
+return 1 if ($hm && $hm->[2] ne 'xfs');
 if ($config{'mail_quotas'}) {
 	my ($mm) = grep { $_->[0] eq $config{'mail_quotas'} } @mounted;
-	return 1 if ($mm && $mm->[2] =~ /^ext/);
+	return 1 if ($mm && $mm->[2] ne 'xfs');
 	}
 return 0;
 }
