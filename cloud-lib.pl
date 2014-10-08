7d3c43ab394350b090c367ffcd1b5c0101757fb3
Jamie Cameron <jcameron@webmin.com>
2014-10-07 19:46:30 -0700

    fix typo

--- cloud-lib.pl
+++ cloud-lib.pl
@@ -5,17 +5,60 @@
 sub list_cloud_providers
 {
 return ( { 'name' => 's3',
+	   'prefix' => [ 's3', 's3rrs' ],
 	   'desc' => $text{'cloud_s3desc'} },
 	 { 'name' => 'rs',
+	   'prefix' => [ 'rs' ],
 	   'desc' => $text{'cloud_rsdesc'} },
        );
 }
 
-sub show_cloud_provider_form
+# backup_uses_cloud(&backup, &provider)
+# Checks if any dest of a backup uses this provider
+sub backup_uses_cloud
+{
+my ($backup, $prov) = @_;
+my @rv;
+foreach my $d (&get_scheduled_backup_dests($backup)) {
+	foreach my $p (@{$prov->{'prefix'}}) {
+		if ($d =~ /^\Q$p\E:/) {
+			push(@rv, $d);
+			last;
+			}
+		}
+	}
+return wantarray ? @rv : $rv[0];
+}
+
+sub cloud_s3_get_state
+{
+if ($config{'s3_akey'}) {
+	return { 'ok' => 1,
+		 'desc' => &text('cloud_s3account', $config{'s3_akey'}),
+	       };
+	}
+else {
+	return { 'ok' => 0 };
+	}
+}
+
+sub cloud_rs_get_state
+{
+if ($config{'rs_user'}) {
+	return { 'ok' => 1,
+		 'desc' => &text('cloud_rsuser', $config{'rs_user'}),
+	       };
+	}
+else {
+	return { 'ok' => 0 };
+	}
+}
+
+sub cloud_google_get_state
 {
 }
 
-sub parse_cloud_provider_form
+sub cloud_dropbox_get_state
 {
 }
 
