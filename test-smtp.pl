c30a2bfc47da43ae32ffbef7e3de5b57fb99b034
Jamie Cameron <jcameron@webmin.com>
2015-01-11 11:19:22 -0800

    Use new API for testing SMTP connections, and add support for SSL mode

--- test-smtp.pl
+++ test-smtp.pl
@@ -18,6 +18,11 @@ which must be followed a username and password respectively. The C<--auth>
 flag can be used to set the SMTP authentication type, which defaults to
 C<Plain>.
 
+By default this command will use an unencrypted connection, but you can force
+a switch to SSL mode with the C<--ssl> flag. Also, the C<--port> flag followed
+by a port number or name can be used to have the SMTP connection made on a port
+other than the default (25 or 465).
+
 =cut
 
 package virtual_server;
@@ -40,7 +45,6 @@ if (!$module_name) {
 $server = "localhost";
 $from = "nobody\@virtualmin.com";
 $auth = "Plain";
-$port = 25;
 while(@ARGV > 0) {
 	local $a = shift(@ARGV);
 	if ($a eq "--server") {
@@ -69,6 +73,9 @@ while(@ARGV > 0) {
 	elsif ($a eq "--auth") {
 		$auth = shift(@ARGV);
 		}
+	elsif ($a eq "--ssl") {
+		$ssl = 1;
+		}
 	elsif ($a eq "--data") {
 		$datafile = shift(@ARGV);
 		}
@@ -80,14 +87,34 @@ while(@ARGV > 0) {
 		}
 	}
 $to || &usage("No destination address specified");
+$port ||= ($ssl ? 465 : 25);
 
 # Open SMTP connection
-&foreign_require("mailboxes", "mailboxes-lib.pl");
+&foreign_require("mailboxes");
 $main::error_must_die = 1;
 eval {
-	&mailboxes::open_socket($server, $port, mailboxes::MAIL);
-	&mailboxes::smtp_command(mailboxes::MAIL);
-	&mailboxes::smtp_command(mailboxes::MAIL, "helo ".&get_system_hostname()."\r\n");
+	my $h = { 'fh' => 'MAIL' };
+	&open_socket($server, $port, $h->{'fh'});
+        if ($ssl) {
+		# Switch to SSL mode
+		eval "use Net::SSLeay";
+                $@ && die($text{'link_essl'});
+                eval "Net::SSLeay::SSLeay_add_ssl_algorithms()";
+                eval "Net::SSLeay::load_error_strings()";
+                $h->{'ssl_ctx'} = Net::SSLeay::CTX_new() ||
+                        die("Failed to create SSL context");
+                $h->{'ssl_con'} = Net::SSLeay::new($h->{'ssl_ctx'}) ||
+                        die("Failed to create SSL connection");
+                Net::SSLeay::set_fd($h->{'ssl_con'}, fileno(MAIL));
+                Net::SSLeay::connect($h->{'ssl_con'}) ||
+                        die("SSL connect() failed");
+                }
+
+	my $callpkg = (caller(0))[0];
+        $h->{'fh'} = $callpkg."::".$h->{'fh'};
+
+	&mailboxes::smtp_command($h);
+	&mailboxes::smtp_command($h, "helo ".&get_system_hostname()."\r\n");
 
 	if ($user) {
 		# Login to SMTP server
@@ -102,7 +129,7 @@ eval {
 						'pass' => $pass } );
 		&error("Failed to create Authen::SASL object") if (!$sasl);
 		local $conn = $sasl->client_new("smtp", &get_system_hostname());
-		local $arv = &mailboxes::smtp_command(mailboxes::MAIL, "auth $auth\r\n", 1);
+		local $arv = &mailboxes::smtp_command($h, "auth $auth\r\n", 1);
 		if ($arv =~ /^(334)\s+(.*)/) {
 			# Server says to go ahead
 			$extra = $2;
@@ -111,13 +138,13 @@ eval {
 			if ($initial) {
 				local $enc = &encode_base64($initial);
 				$enc =~ s/\r|\n//g;
-				$arv = &mailboxes::smtp_command(mailboxes::MAIL, "$enc\r\n", 1);
+				$arv = &mailboxes::smtp_command($h, "$enc\r\n", 1);
 				if ($arv =~ /^(\d+)\s+(.*)/) {
 					if ($1 == 235) {
 						$auth_ok = 1;
 						}
 					else {
-						&error("Unknown SMTP authentication response : $arv");
+						die("Unknown SMTP authentication response : $arv");
 						}
 					}
 				$extra = $2;
@@ -127,36 +154,38 @@ eval {
 				local $return = $conn->client_step($message);
 				local $enc = &encode_base64($return);
 				$enc =~ s/\r|\n//g;
-				$arv = &mailboxes::smtp_command(mailboxes::MAIL, "$enc\r\n", 1);
+				$arv = &mailboxes::smtp_command($h, "$enc\r\n", 1);
 				if ($arv =~ /^(\d+)\s+(.*)/) {
 					if ($1 == 235) {
 						$auth_ok = 1;
 						}
 					elsif ($1 == 535) {
-						&error("SMTP authentication failed : $arv");
+						die("SMTP authentication failed : $arv");
 						}
 					$extra = $2;
 					}
 				else {
-					&error("Unknown SMTP authentication response : $arv");
+					die("Unknown SMTP authentication response : $arv");
 					}
 				}
 			}
 		}
 
 	# Send from and to
-	&mailboxes::smtp_command(mailboxes::MAIL, "mail from: <$from>\r\n");
-	&mailboxes::smtp_command(mailboxes::MAIL, "rcpt to: <$to>\r\n");
+	&mailboxes::smtp_command($h, "mail from: <$from>\r\n");
+	&mailboxes::smtp_command($h, "rcpt to: <$to>\r\n");
 	if ($datafile) {
 		$data = &read_file_contents($datafile);
 		$data || die "Failed to read $datafile : $!";
 		$data =~ s/^From\s.*\r?\n//;	# Remove From line
-		$r = &mailboxes::smtp_command(mailboxes::MAIL, "data\r\n");
-		print mailboxes::MAIL $data;
-		$r = &mailboxes::smtp_command(mailboxes::MAIL, ".\r\n");
+		$r = &mailboxes::smtp_command($h, "data\r\n");
+		$data =~ s/\r\n/\n/g;
+		$data =~ s/\n/\r\n/g;
+		&write_http_connection($h, $data);
+		$r = &mailboxes::smtp_command($h, ".\r\n");
 		}
-	&mailboxes::smtp_command(mailboxes::MAIL, "quit\r\n");
-	close(MAIL);
+	&mailboxes::smtp_command($h, "quit\r\n");
+	&close_http_connection($h);
 	};
 if ($@) {
 	$err = &entities_to_ascii(&html_tags_to_text("$@"));
@@ -176,6 +205,7 @@ print "\n";
 print "virtualmin test-smtp --to address\n";
 print "                    [--server hostname]\n";
 print "                    [--port number|name]\n";
+print "                    [--ssl]\n";
 print "                    [--from address]\n";
 print "                    [--user login --pass password]\n";
 print "                    [--auth method]\n";
