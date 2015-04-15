8a5104cbbaa64a0df538341486ee50902f0f80d2
Jamie Cameron <jcameron@webmin.com>
2015-04-14 21:59:28 -0700

    New phpMyAdmin version

--- scripts/phpmyadmin.pl
+++ scripts/phpmyadmin.pl
@@ -19,7 +19,7 @@ return "A browser-based MySQL database management interface.";
 # script_phpmyadmin_versions()
 sub script_phpmyadmin_versions
 {
-return ( "4.4.1.1", "4.0.10.9", "3.5.8.2" );
+return ( "4.4.2", "4.0.10.9", "3.5.8.2" );
 }
 
 sub script_phpmyadmin_can_upgrade
