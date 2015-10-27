e5940aa7fc33d2c17d036eb5d73607966156de65
Jamie Cameron <jcameron@webmin.com>
2015-10-26 20:32:55 -0700

    SCL repo's PHP 5.6 uses paths like /opt/rh/rh-php56/...

--- php-lib.pl
+++ php-lib.pl
@@ -763,10 +763,12 @@ foreach my $v (@all_possible_php_versions) {
 		$phpn ||= &has_command("php$nodotv-cgi") ||
 			  &has_command("php-cgi$nodotv") ||
 			  &has_command("/opt/rh/php$nodotv/root/usr/bin/php-cgi") ||
+			  &has_command("/opt/rh/rh-php$nodotv/root/usr/bin/php-cgi") ||
 			  &has_command("/opt/atomic/atomic-php$nodotv/root/usr/bin/php") ||
 			  &has_command("/opt/rh/php$nodotv/bin/php-cgi") ||
 			  &has_command("php$nodotv") ||
 			  &has_command("/opt/rh/php$nodotv/root/usr/bin/php");
+			  &has_command("/opt/rh/rh-php$nodotv/root/usr/bin/php");
 			  &has_command("/opt/rh/php$nodotv/bin/php") ||
 			  &has_command(glob("/opt/phpfarm/inst/bin/php-cgi-$v.*"));
 		}
