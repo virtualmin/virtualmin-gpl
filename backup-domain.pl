53d524c3bb9e4c64dd067e67fe8a2ffa9a72af02
Jamie Cameron <jcameron@webmin.com>
2016-01-24 21:31:16 -0800

    Document all supported backup destination URLs

--- backup-domain.pl
+++ backup-domain.pl
@@ -436,6 +436,8 @@ print " - An FTP destination, like ftp://login:pass\@server/backup/yourdomain.co
 print " - An SSH destination, like ssh://login:pass\@server/backup/yourdomain.com.tgz\n";
 print " - An S3 bucket, like s3://accesskey:secretkey\@bucket\n";
 print " - A Rackspace container, like rs://user:apikey\@container\n";
+print " - A Google Cloud Storage bucket, like gcs://bucket\n";
+print " - A Dropbox folder, like dropbox://folder\n";
 print "Multiple destinations can be given, if they are all remote.\n";
 exit(1);
 }
