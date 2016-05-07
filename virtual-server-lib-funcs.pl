e85b96f79ab720f5c18642c3b137e7d26851a870
Jamie Cameron <jcameron@webmin.com>
2016-05-07 16:09:58 -0700

    If the db field is somehow missing, fill it in https://www.virtualmin.com/node/40618

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -272,6 +272,9 @@ if (!defined($dom->{'plan'}) && !$main::no_auto_plan) {
 	local @plans = sort { $a->{'id'} <=> $b->{'id'} } &list_plans();
 	$dom->{'plan'} = $plans[0]->{'id'};
 	}
+if ($dom->{'db'} eq '') {
+	$dom->{'db'} = &database_name($dom);
+	}
 if (!defined($dom->{'db_mysql'}) && $dom->{'mysql'}) {
 	# Assume just one MySQL DB
 	$dom->{'db_mysql'} = $dom->{'db'};
