47d5afb65e471f2ddbd2e950b1de28672b76a82a
Jamie Cameron <jcameron@webmin.com>
2017-01-06 16:59:19 -0800

    Use standard function

--- set-global-feature.pl
+++ set-global-feature.pl
@@ -35,11 +35,7 @@ if (!$module_name) {
 	$< == 0 || die "set-global-feature.pl must be run as root";
 	}
 @OLDARGV = @ARGV;
-
-$first_print = \&first_text_print;
-$second_print = \&second_text_print;
-$indent_print = \&indent_text_print;
-$outdent_print = \&outdent_text_print;
+&set_all_text_print();
 
 # Parse command-line args
 while(@ARGV > 0) {
