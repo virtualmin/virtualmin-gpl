7339556aefd12c4ee384eafba6d26466d74e24d4
Jamie Cameron <jcameron@webmin.com>
2020-08-12 21:04:00 -0700

    Fix CA cert key

--- install-cert.pl
+++ install-cert.pl
@@ -134,7 +134,7 @@ foreach $g (@got) {
 	my $k = $g->[0] eq 'ca' ? 'chain' : $g->[0];
 	$d->{'ssl_'.$k} ||= &default_certificate_file($d, $g->[0]);
 	&lock_file($d->{'ssl_'.$k});
-	&open_tempfile_as_domain_user($d, SSL, ">".$d->{'ssl_'.$g->[0]});
+	&open_tempfile_as_domain_user($d, SSL, ">".$d->{'ssl_'.$k});
 	&print_tempfile(SSL, $g->[1]);
 	&close_tempfile_as_domain_user($d, SSL);
 	&set_certificate_permissions($d, $d->{'ssl_'.$k});
