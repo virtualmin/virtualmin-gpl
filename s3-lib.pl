430cd2ed2f7118bdeacce06e0baaa80e9f5ed645
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2021-02-01 15:28:19 -0300

    Update hardcoded S3 regions list

--- s3-lib.pl
+++ s3-lib.pl
@@ -984,8 +984,10 @@ return (1, $rheader{'etag'});
 sub s3_list_locations
 {
 my ($akey, $skey) = @_;
-return ( "us-west-1", "us-west-2", "EU", "ap-southeast-1", "ap-southeast-2",
-	 "ap-south-1", "ap-northeast-1", "sa-east-1", "eu-central-1" );
+return ("us-east-1", "us-east-2", "us-west-1", "us-west-2", "af-south-1",
+	"ap-east-1", "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2",
+	"ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2",
+	"eu-south-1", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1");
 }
 
 # can_use_aws_cmd(access-key, secret-key)
