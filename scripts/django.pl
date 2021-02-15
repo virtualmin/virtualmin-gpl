f7672c4c3d6fd80d57d67752b12e695e4ae9a947
Jamie Cameron <jcameron@webmin.com>
2021-02-14 21:04:39 -0800

    Fix version detection

--- scripts/django.pl
+++ scripts/django.pl
@@ -492,9 +492,9 @@ sub script_django_latest
 {
 local ($ver) = @_;
 return ( "http://www.djangoproject.com/download/",
-	 $ver >= 1.9 ? "Django-([0-9\\.]+)\\.tar\\.gz" :
-	 $ver >= 1.7 ? "Django-(1\\.7\\.[0-9\\.]+)\\.tar\\.gz" 
-		     : "Django-(1\\.4\\.[0-9\\.]+)\\.tar\\.gz" );
+	 $ver >= 3.1 ? "Django-([0-9\\.]+)\\.tar\\.gz" :
+	 $ver >= 2.2 ? "Django-(2\\.[0-9\\.]+)\\.tar\\.gz" 
+		     : "Django-(1\\.[0-9\\.]+)\\.tar\\.gz" );
 }
 
 sub script_django_site
