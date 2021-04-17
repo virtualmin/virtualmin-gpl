0b4813cdffbfbf3441762a4eb9e712fbae79a8b7
Ilia Rostovtsev <ilia@rostovtsev.io>
2021-04-18 01:49:45 +0300

    Fix to restore accidentally deleted code

--- scripts/whmcs.pl
+++ scripts/whmcs.pl
@@ -518,15 +518,14 @@ if ($opts->{'newdb'}) {
 return (1, "WHMCS directory and tables deleted.");
 }
 
-
-#sub script_whmcs_latest
-#{
-#local ($ver) = @_;
-#local $sfx = $ver =~ /^(\d+)\.(\d+)\.(\d+)$/ ? ".$3" : "";
-#return ( "http://www.whmcs.com/whats-new/#download",
-#	 "WHMCS\\s+V(\\S+)\\s+Stable",
-#	 undef, $sfx );
-#}
+sub script_whmcs_latest
+{
+local ($ver) = @_;
+return ( "https://docs.whmcs.com/Release_Notes",
+	 &compare_versions($ver, 7) < 0 ?
+		"Version_(6\\.[0-9\.]+)_Release" :
+		"Version_([0-9\.]+)_Release" );
+}
 
 sub script_whmcs_site
 {
