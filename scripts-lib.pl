1f98eddad1e99c4e56fe1534ed60c637d314987f
Jamie Cameron <jcameron@webmin.com>
2021-05-05 21:48:36 -0700

    Fail script installs if they need PHP but it's disabled

--- scripts-lib.pl
+++ scripts-lib.pl
@@ -2757,6 +2757,14 @@ sub check_script_depends
 local ($script, $d, $ver, $sinfo, $phpver) = @_;
 local @rv;
 
+# If the script uses PHP, make sure it's enabled for the domain
+if (&indexof("php", @{$script->{'uses'}}) >= 0) {
+	local $mode = &get_domain_php_mode($d);
+	if ($mode eq "none") {
+		push(@rv, $text{'scripts_iphpneed'});
+		}
+	}
+
 # Call script's depends function
 if (defined(&{$script->{'depends_func'}})) {
 	push(@rv, grep { $_ } &{$script->{'depends_func'}}($d, $ver, $sinfo, $phpver));
