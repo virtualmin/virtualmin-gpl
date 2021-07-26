c933f14f44d3c3df4fb2bfae8f66744ea516d429
Jamie Cameron <jcameron@webmin.com>
2021-07-25 18:28:44 -0700

    Set FPM limit on max number of processes

--- php-lib.pl
+++ php-lib.pl
@@ -843,6 +843,21 @@ foreach my $dir ("$d->{'home'}/fcgi-bin", &cgi_bin_dir($d)) {
 	}
 }
 
+# set_php_fpm_ulimits(&domain, &resource-limits)
+# Update the FPM config with resource limits
+sub set_php_fpm_ulimits
+{
+my ($d, $res) = @_;
+my $conf = &get_php_fpm_config($d);
+return 0 if (!$conf);
+if ($res->{'procs'}) {
+	&save_php_fpm_config_value($d, "process.max", $res->{'procs'});
+	}
+else {
+	&save_php_fpm_config_value($d, "process.max", undef);
+	}
+}
+
 # supported_php_modes([&domain])
 # Returns a list of PHP execution modes possible for a domain
 sub supported_php_modes
