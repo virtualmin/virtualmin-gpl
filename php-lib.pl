0fcbea31f3dbed02935f84fee675712ca505c0f1
Jamie Cameron <jcameron@webmin.com>
2021-12-03 20:36:41 -0800

    Setting permissions too wide is a security risk https://github.com/virtualmin/virtualmin-gpl/issues/335

--- php-lib.pl
+++ php-lib.pl
@@ -2145,7 +2145,6 @@ else {
 		   "user = ".$d->{'user'},
 		   "group = ".$d->{'ugroup'},
 		   "listen = ".$port,
-		   "listen.mode = 0600",
 		   "pm = dynamic", 
 		   "pm.max_children = $defchildren",
 		   "pm.start_servers = 1",
