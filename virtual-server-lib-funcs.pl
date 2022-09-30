1af6d0e92a88070c2d1f4b11b4952cf689bd3bbb
iliajie <gpg@ilia.engineer>
2022-09-30 23:43:23 +0300

    Fix to also set env vars old way

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -4836,6 +4836,7 @@ foreach my $v (&get_global_template_variables()) {
 # Set other variables
 if ($others) {
 	foreach my $e (keys %$others) {
+		$ENV{uc($e)} = $others->{$e};
 		$ENV{"VIRTUALSERVER_".uc($e)} = $others->{$e};
 		}
 	}
