1da08712f626f5d39dc380b1de291866c369970b
iliajie <gpg@ilia.engineer>
2022-10-15 19:18:19 +0300

    Fix to use API to get helper command

--- upgrade-licence.pl
+++ upgrade-licence.pl
@@ -56,7 +56,7 @@ my ($out, $err);
 
 # Setup Virtualmin license and repositories
 &$first_print("Upgrading Virtualmin license and repositories ..");
-my $vmcmd = &has_command('virtualmin');
+my $vmcmd = &get_api_helper_command(get_api_helper_command);
 $vmcmd || &usage('Cannot find Virtualmin helper command');
 &execute_command("$vmcmd setup-repos ".
 	"--serial @{[quotemeta($serial)]} --key @{[quotemeta($key)]} ".
