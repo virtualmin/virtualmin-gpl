d0073cfd73cfda84514e77633b9d84ed66050a00
Jamie Cameron <jcameron@webmin.com>
2022-11-18 22:05:07 -0800

    PHP wrappers template is basically never used

--- virtual-server-lib.pl
+++ virtual-server-lib.pl
@@ -91,8 +91,7 @@ $config{'virt6'} = 1;
 @subdom_features = ( @opt_subdom_features );
 @database_features = ( 'mysql', 'postgres' );
 @template_features = ( 'basic', 'resources', @features, 'virt', 'virtualmin',
-		       'plugins', 'scripts', 'autoconfig',
-		       'php', 'phpwrappers', 'avail' );
+		       'plugins', 'scripts', 'autoconfig', 'php', 'avail' );
 @template_features_effecting_webmin = ( 'web', 'webmin', 'avail' );
 @can_always_features = ( 'dir', 'unix', 'logrotate' );
 @validate_features = ( @features, "virt", "virt6" );
