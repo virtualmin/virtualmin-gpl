d858dd699e278d4c817d79e545c4129ee9ff2236
Jamie Cameron <jcameron@webmin.com>
2023-01-02 18:35:29 -0800

    Don't try to remove DNS records when cleaning up DKIM at deletion time, as DNS is already gone

--- feature-mail.pl
+++ feature-mail.pl
@@ -577,7 +577,7 @@ if ($supports_dependent) {
 &delete_everyone_file($d);
 
 # Remove domain from DKIM list
-&update_dkim_domains($d, 'delete', $leave_dns);
+&update_dkim_domains($d, 'delete', $leave_dns || $d->{'deleting'});
 
 # Remove secondary virtusers from slaves
 &sync_secondary_virtusers($d);
