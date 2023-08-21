3c26248b418b2c9d2ddec553fecedeb3df44e622
Jamie Cameron <jcameron@webmin.com>
2023-08-20 18:46:19 -0700

    Start of API command to restart managed servers

---
+++ restart-server.pl
@@ -0,0 +1,110 @@
+#!/usr/local/bin/perl
+
+=head1 restart-server.pl
+
+Restarts one of the servers managed by Virtualmin.
+
+This command stops and re-starts one of the servers managed by Virtualmin,
+such as Apache or BIND. The server to restart must be set using the
+C<--server> flag, followed by a feature name like C<web> or C<dns>.
+
+=cut
+
+package virtual_server;
+if (!$module_name) {
+	$main::no_acl_check++;
+	$ENV{'WEBMIN_CONFIG'} ||= "/etc/webmin";
+	$ENV{'WEBMIN_VAR'} ||= "/var/webmin";
+	if ($0 =~ /^(.*)\/[^\/]+$/) {
+		chdir($pwd = $1);
+		}
+	else {
+		chop($pwd = `pwd`);
+		}
+	$0 = "$pwd/restart-server.pl";
+	require './virtual-server-lib.pl';
+	$< == 0 || die "restart-server.pl must be run as root";
+	}
+@OLDARGV = @ARGV;
+&set_all_text_print();
+
+# Parse command-line args
+while(@ARGV > 0) {
+	local $a = shift(@ARGV);
+	if ($a eq "--domain") {
+		$dname = shift(@ARGV);
+		}
+	elsif ($a eq "--server") {
+		$sname = shift(@ARGV);
+		}
+	elsif ($a eq "--multiline") {
+		$multiline = 1;
+		}
+	elsif ($a eq "--help") {
+		&usage();
+		}
+	else {
+		&usage("Unknown parameter $a");
+		}
+	}
+
+# Validate server name
+$sname || &usage("Missing name of server to restart");
+@slist = ( );
+foreach my $f (@startstop_features) {
+	my $sfunc = "startstop_".$f;
+	if ($config{$f} && defined(&$sfunc)) {
+		if ($f eq $sname) {
+			$found = 1;
+			}
+		push(@slist, $f);
+		}
+	}
+foreach my $f (&list_startstop_plugins()) {
+	if ($f eq $sname) {
+		$found = 2;
+		}
+	push(@slist, $f);
+	}
+$found || &usage("Server $sname does not exist. Valid servers are : ".join(" ", @slist));
+
+# Restart the server
+&$first_print("Restarting server type $sname ...");
+if ($found == 1) {
+	# Core server
+	# XXX get FPM version
+	my $startfunc = "start_service_".$sname;
+	my $stopfunc = "stop_service_".$sname;
+	$err = &$stopfunc($ver);
+	if (!$err) {
+		$err = &$startfunc($ver);
+		}
+	}
+else {
+	# Plugin server
+	$err = &plugin_call($sname, "feature_stop_service", $ver);
+	if (!$err) {
+		$err = &plugin_call($sname, "feature_start_service", $ver);
+		}
+	}
+if ($err) {
+	&$second_print(".. failed : $err");
+	}
+else {
+	&$second_print(".. done");
+	}
+
+&run_post_actions();
+&virtualmin_api_log(\@OLDARGV);
+exit($err ? 1 : 0);
+
+sub usage
+{
+print "$_[0]\n\n" if ($_[0]);
+print "Restarts one of the servers managed by Virtualmin.\n";
+print "\n";
+print "virtualmin restart-server --server name\n";
+print "                         [--domain name]\n";
+exit(1);
+}
+
