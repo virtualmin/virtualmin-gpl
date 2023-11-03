d60b09306eee697ffd04bf992493d54c2a70e947
Jamie Cameron <jcameron@webmin.com>
2023-11-03 00:48:00 -0700

    New django version

--- scripts/django.pl
+++ scripts/django.pl
@@ -18,7 +18,7 @@ return "Django is a high-level Python Web framework that encourages rapid develo
 # script_django_versions()
 sub script_django_versions
 {
-return ( "4.2.7", "3.2.22", "2.2.28" );
+return ( "4.2.7", "3.2.23", "2.2.28" );
 }
 
 sub script_django_can_upgrade
