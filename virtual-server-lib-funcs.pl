b52b5283a3082924164f6cedbbd3b2bb44e59f1a
Ilia Ross <ilia@ross.gdn>
2024-05-14 19:55:57 +0300

    Fix to print call stack only if enabled

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -576,7 +576,7 @@ if (&read_file($file, $oldd)) {
 	my @st = stat($file);
 	if ($d->{'lastread_time'} && $st[9] > $d->{'lastread_time'}) {
 		print STDERR "Domain $file was modified since last read!\n";
-		&print_call_stack();
+		&print_call_stack() if ($gconfig{'error_stack'});
 		}
 	}
 if (!$d->{'created'}) {
