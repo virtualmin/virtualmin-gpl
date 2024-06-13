533c898a6fa729904d7a2b05fdaad0a2ae95ec6d
Jamie Cameron <jcameron@webmin.com>
2024-06-13 13:39:20 -0700

    Take a long line

--- s3-lib.pl
+++ s3-lib.pl
@@ -1103,7 +1103,14 @@ return ();
 # Returns locations supported by Amazon S3
 sub s3_list_aws_locations
 {
-return ( "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-2", "ap-southeast-3", "ap-southeast-4", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "ca-west-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-south-2", "eu-north-1", "eu-central-2", "il-central-1", "me-south-1", "me-central-1", "sa-east-1", "us-gov-east-1", "us-gov-west-1");
+return ( "us-east-1", "us-west-1", "us-west-2", "af-south-1", "ap-east-1",
+	 "ap-south-2", "ap-southeast-3", "ap-southeast-4", "ap-south-1",
+	 "ap-northeast-3", "ap-northeast-2", "ap-southeast-1",
+	 "ap-southeast-2", "ap-northeast-1", "ca-central-1", "ca-west-1",
+	 "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3",
+	 "eu-south-2", "eu-north-1", "eu-central-2", "il-central-1",
+	 "me-south-1", "me-central-1", "sa-east-1", "us-gov-east-1",
+	 "us-gov-west-1" );
 }
 
 # can_use_aws_s3_creds()
