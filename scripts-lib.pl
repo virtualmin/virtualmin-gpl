6e2b03b0537558b89775bf1079f0e2ee5f0b7e4c
Ilia Ross <ilia@ross.gdn>
2024-06-20 03:03:38 +0300

    Add ability to test command if it starts with the path

--- scripts-lib.pl
+++ scripts-lib.pl
@@ -3350,7 +3350,8 @@ my ($ver) = @_;
 my $basever = substr($ver, 0, 1);
 my @opts = ( $config{'python_cmd'} );
 if (!$basever || $basever == 3) {
-	push(@opts, "python3.14", "python314",
+	push(@opts, "/etc/alternatives/python3",
+		    "python3.14", "python314",
 		    "python3.13", "python313",
 		    "python3.12", "python312",
 		    "python3.11", "python311",
@@ -3362,13 +3363,22 @@ if (!$basever || $basever == 3) {
 		    "python3", "python30");
 	}
 if (!$basever || $basever == 2) {
-	push(@opts, "python2.7", "python27",
+	push(@opts, "/etc/alternatives/python2",
+		    "python2.7", "python27",
 		    "python2.6", "python26",
 		    "python2", "python20");
 	}
 push(@opts, "python");
 foreach my $o (@opts) {
-	my $p = &has_command($o);
+	my $p;
+	if ($o =~ /^\//) {
+		my ($path, $cmd) = ($o =~ m|^(.*)/([^/]+)$|);
+		local $ENV{'PATH'} = $path;
+		$p = &has_command($cmd);
+		}
+	else {
+		$p = &has_command($o);
+		}
 	next if (!$p);
 	next if ($ver && &get_python_version($p) !~ /^\Q$ver\E(\.|$)/);
 	return $p;
