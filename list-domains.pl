d29510c6f65f9e6b0ce4825594d926f27df982d6
Jamie Cameron <jcameron@webmin.com>
2024-06-20 20:31:17 -0700

    API commands should use a consistent date format

--- list-domains.pl
+++ list-domains.pl
@@ -933,11 +933,10 @@ if ($multi) {
 		# Last login
 		if ($config{'show_domains_lastlogin'}) {
 			if ($d->{'last_login_timestamp'}) {
-				print "    Last login: ".
-					&human_readable_time(
-					    $d->{'last_login_timestamp'})."\n";
+				print "    Last login: ",
+				  &make_date($d->{'last_login_timestamp'}),"\n";
 				print "    Last login time: ",
-					"$d->{'last_login_timestamp'}\n";
+				  $d->{'last_login_timestamp'},"\n";
 				}
 			else {
 				print "    Last login: Never\n";
