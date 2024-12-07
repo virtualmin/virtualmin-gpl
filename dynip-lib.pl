de028b323f9d67e35cb15b05b95d875b3d14c82e
Ilia Ross <ilia@ross.gdn>
2024-12-07 19:52:40 +0200

    Fix to sync with master and resolve MR conflict

--- dynip-lib.pl
+++ dynip-lib.pl
@@ -49,17 +49,22 @@ else {
 # Returns the IP address of this system, as seen by other hosts on the Internet.
 # If the no-cache flag is set, the IP is always fetched from the network. If the
 # type is set to 6 ("ipv6"), an IPv6 address is returned. Default is 4 (IPv4).
+# For DNS resolver to work, the config option "dns_resolver" must be set to a
+# string like "myip.opendns.com resolver1.opendns.com".
 sub get_external_ip_address
 {
 my ($nocache, $type) = @_;
+my ($out, $error);
+my $timeout = 5;
 # Validate type
 $type = 4 if (!$type || ($type != 4 && $type != 6));
 # Internal sub to validate an IP address of the correct type
 my $ip = sub {
 	my $ipaddr = shift;
 	return undef if (!$ipaddr);
-	return $ipaddr if ($type == 4 && $ipaddr =~ /^(\d+\.\d+\.\d+\.\d+)$/);
-	return $ipaddr if ($type == 6 && $ipaddr =~ /^([0-9a-fA-F:]+)$/);
+	$ipaddr =~ s/\r|\n//g;
+	return $ipaddr if ($type == 4 && &check_ipaddress($ipaddr));
+	return $ipaddr if ($type == 6 && &check_ip6address($ipaddr));
 	return undef;
 	};
 my $now = time();
@@ -72,13 +77,29 @@ if (!$nocache && $config{$cache_optname} &&
 	# Can use last cached value
 	return $ip->($config{$cache_optname});
 	}
-my $url = "http://software.virtualmin.com/cgi-bin/ip.cgi";
-my ($host, $port, $page, $ssl) = &parse_http_url($url);
-my ($out, $error);
-&http_download($host, $port, $page, \$out, \$error, undef, $ssl,
-	       undef, undef, 5, 0, 1);
-$out =~ s/\r|\n//g;
-$out = $ip->($out);
+# Fetch IP using DNS
+if ((my $dig = &has_command("dig")) &&
+    $config{'dns_resolver'} =~ /^(?<qname>\S+)\s+(?<nserv>\S+)$/) {
+	my $qname = quotemeta($+{qname});
+	my $nserv    = quotemeta($+{nserv});
+	my $dig_cmd = "$dig +time=$timeout +short -".($type == 6 ? "6" : "4").
+		      " $qname \@" . $nserv;
+	&execute_command($dig_cmd, undef, \$out, \$error);
+	$out = $ip->($out);
+	}
+# Fetch IP using http
+if ($error || !$out) {
+	my $url = $config{'ip_lookup_url'} || 
+		"http://software.virtualmin.com/cgi-bin/ip.cgi";
+	my $url4 = $url;
+	my $url6 = $url;
+	($url4, $url6) = split(/ /, $url) if ($url =~ / /);
+	$url = $type == 4 ? $url4 : $url6;
+	my ($host, $port, $page, $ssl) = &parse_http_url($url);
+	&http_download($host, $port, $page, \$out, \$error, undef, $ssl,
+		undef, undef, $timeout, 0, 1);
+	$out = $ip->($out);
+	}
 if ($error) {
 	# Fall back to last cached value
 	return $ip->($config{$cache_optname});
