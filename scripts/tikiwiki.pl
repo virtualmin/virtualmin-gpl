726f6e80ea7cfa702814cd4c37a9d067e2912393
Ilia Ross <ilia@ross.gdn>
2025-03-06 00:56:14 +0200

    Update Tiki to latest version 28.1

--- scripts/tikiwiki.pl
+++ scripts/tikiwiki.pl
@@ -18,7 +18,7 @@ return "A full featured free software Wiki/CMS/Groupware written in PHP";
 # script_tikiwiki_versions()
 sub script_tikiwiki_versions
 {
-return ( "27.1", "24.7", "21.11" );
+return ( "28.1", "27.1", "24.7", "21.11" );
 }
 
 sub script_tikiwiki_release
