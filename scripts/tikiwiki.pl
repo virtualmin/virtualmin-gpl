115df4f1b82f35ac9778ef222f4bea419ac920b0
Jamie Cameron <jcameron@webmin.com>
2025-04-12 20:50:38 -0700

    Tikiwiki 21 is EOL
    https://github.com/virtualmin/virtualmin-gpl/issues/1047

--- scripts/tikiwiki.pl
+++ scripts/tikiwiki.pl
@@ -18,7 +18,7 @@ return "A full featured free software Wiki/CMS/Groupware written in PHP";
 # script_tikiwiki_versions()
 sub script_tikiwiki_versions
 {
-return ( "28.3", "27.2", "24.8", "21.12" );
+return ( "28.3", "27.2", "24.8" );
 }
 
 sub script_tikiwiki_release
