25fee2639a87b69a7ebb9bbc1cd8a97e85a6696f
Ilia Ross <ilia@ross.gdn>
2025-04-28 15:29:56 +0300

    Fix to use Webmin base version only

--- postinstall.pl
+++ postinstall.pl
@@ -576,7 +576,9 @@ if ($config{'rs_endpoint'} eq 'https://lon.auth.api.rackspacecloud.com/v1.0') {
 &unlock_file($module_config_file);
 
 # Enable Webmin forgotten password recovery, if supported
-if (&get_webmin_version() >= 2.304 && !defined($gconfig{'forgot_pass'})) {
+my $webmin_version = &get_webmin_version();
+$webmin_version =~ s/^(\d+\.\d+).*/$1/;
+if ($webmin_version >= 2.304 && !defined($gconfig{'forgot_pass'})) {
 	&lock_file("$config_directory/config");
 	$gconfig{'forgot_pass'} = 1;
 	&write_file("$config_directory/config", \%gconfig);
