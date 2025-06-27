17ce685157c64c608294bd6cb0866481e1b2ac67
Ilia Ross <ilia@ross.gdn>
2025-06-28 01:31:01 +0300

    Fix to put alias domain next to parent
    https://forum.virtualmin.com/t/le-ssl-certificate-generating-failure-and-virtual-host-paths-incorrect/133929/5?u=ilia

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -154,6 +154,10 @@ if ($sortfield eq 'user' || $sortfield eq 'sub') {
 	local @catdoms;
 	foreach my $d (grep { !$_->{'parent'} } @doms) {
 		push(@catdoms, $d);
+		foreach my $ad (grep { $_->{'alias'} eq $d->{'id'} } @doms) {
+			$ad->{'indent'} = 2;
+			push(@catdoms, $ad);
+			}
 		foreach my $sd (grep { $_->{'parent'} eq $d->{'id'} &&
 				       !$_->{'alias'} } @doms) {
 			$sd->{'indent'} = 1;
@@ -163,10 +167,6 @@ if ($sortfield eq 'user' || $sortfield eq 'sub') {
 				$ad->{'indent'} = 3;
 				push(@catdoms, $ad);
 				}
-			}
-		foreach my $ad (grep { $_->{'alias'} eq $d->{'id'} } @doms) {
-			$ad->{'indent'} = 2;
-			push(@catdoms, $ad);
 			}
 		}
 	# Any domains that we missed due to their parent not being included
