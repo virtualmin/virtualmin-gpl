f5648e8b41be89c0287a1560849b6f6e6a6100b6
Ilia Ross <ilia@ross.gdn>
2025-09-30 21:47:10 +0300

    Fix to stress out which IP type cannot be resolved

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -19396,7 +19396,8 @@ if (defined(&get_reseller)) {
 if ($config{'dns_ip'.$suffix} eq '*') {
 	my $rv = &get_external_ip_address(0, $prefer);
 	$rv = &get_external_ip_address(1, $prefer) if !$rv;
-	$rv || &error($text{'newdynip_eext'});
+	my $lbl = $prefer == 6 ? 'newdynip_eext6' : 'newdynip_eext';
+	$rv || &error($text{$lbl});
 	return $rv;
 	}
 elsif ($config{'dns_ip'.$suffix}) {
