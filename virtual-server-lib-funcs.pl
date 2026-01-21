bd5dc8d2b4ccfc089c9c615a800b4afe4de363d5
Jamie Cameron <jcameron@webmin.com>
2026-01-20 16:55:03 -0800

    It is still legitimate to show the DKIM page even for domains without it currently enabled

--- virtual-server-lib-funcs.pl
+++ virtual-server-lib-funcs.pl
@@ -14099,8 +14099,7 @@ if ($d->{'mail'} && $config{'mail'} && &can_edit_mail()) {
 		    'cat' => 'mail',
 		  });
 	}
-my $has_dkim_enabled = &dkim_domains_file_has_domain($d);
-if ($has_dkim_enabled && !$d->{'alias'} && $config{'dkim_enabled'}) {
+if (!$d->{'alias'} && $config{'dkim_enabled'}) {
 	# Per-domain DKIM page
 	push(@rv, { 'page' => 'edit_domdkim.cgi',
 		    'title' => $text{'edit_domdkim'},
