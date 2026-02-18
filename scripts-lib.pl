af58ab6d84a7705573652dc23380b35b4d71192f
Ilia Ross <ilia@ross.gdn>
2026-02-18 23:09:38 +0200

    Fix newline handling in `add_domain_script` function

--- scripts-lib.pl
+++ scripts-lib.pl
@@ -300,7 +300,7 @@ sub add_domain_script
 {
 local ($d, $name, $version, $opts, $desc, $url, $user, $pass, $partial) = @_;
 $main::add_domain_script_count++;
-$partial =~ s/\n/ /g if ($partial);
+$partial =~ s/[\r\n]+/ /mg if ($partial);
 local %info = ( 'id' => time().$$.$main::add_domain_script_count,
 		'name' => $name,
 		'version' => $version,
