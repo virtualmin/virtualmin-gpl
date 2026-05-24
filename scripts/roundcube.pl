7ef40822796699027746d874f7c427e3e80b62b7
Jamie Cameron <jcameron@webmin.com>
2026-05-24 13:21:26 -0700

    New roundcube version

--- scripts/roundcube.pl
+++ scripts/roundcube.pl
@@ -17,7 +17,7 @@ return "RoundCube Webmail is a browser-based multilingual IMAP client with an ap
 # script_roundcube_versions()
 sub script_roundcube_versions
 {
-return ( "1.7.0", "1.6.15" );
+return ( "1.7.1", "1.6.16" );
 }
 
 sub script_roundcube_version_desc
