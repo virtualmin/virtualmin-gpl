def914bbdca3411c119f1ffb2a11a4fea8a55df3
Ilia Ross <ilia@ross.gdn>
2026-07-01 01:31:47 +0200

    Fix FTP bandwidth reporting after virtual FTP removal

--- virtual-server-lib.pl
+++ virtual-server-lib.pl
@@ -108,7 +108,7 @@ foreach my $fname (@features, "virt", "virt6") {
 @startstop_features = ("web", "dns", "mail", "unix", "virus", "spam",
 		       "mysql", "postgres");
 @startstop_always_features = ("ftp", "fpm");
-@bandwidth_features = ( @features, "backup", "restore" );
+@bandwidth_features = ( @features, "ftp", "backup", "restore" );
 @config_features = grep { $config{$_} } @features;
 @banned_usernames = ( 'root', 'resellers' );
 
